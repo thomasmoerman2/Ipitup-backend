f09011306279177479b05b74f118ebc626af711e
MeanRom <moer.thomas10@gmail.com>
2025-01-20 15:40:16 +0100

    fix cors

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-var AllowSpecificOrigins = "_allowSpecificOrigins";var host = new HostBuilder()    .ConfigureFunctionsWebApplication()    .ConfigureServices(services =>    {        services.AddApplicationInsightsTelemetryWorkerService();        services.ConfigureFunctionsApplicationInsights();        services.AddCors(options =>        {            options.AddPolicy(name: AllowSpecificOrigins,                              builder =>                              {                                  builder.WithOrigins(Environment.GetEnvironmentVariable("AllowedOrigins")).AllowAnyHeader().AllowAnyMethod();                              });        });        services.AddScoped<IUserRepository, UserRepository>();        services.AddScoped<IActivityRepository, ActivityRepository>();        services.AddScoped<IBadgeRepository, BadgeRepository>();        services.AddScoped<ILeaderboardRepository, LeaderboardRepository>();        services.AddScoped<IFriendsRepository, FriendsRepository>();        services.AddScoped<IUserService, UserService>();        services.AddScoped<IActivityService, ActivityService>();        services.AddScoped<IBadgeService, BadgeService>();        services.AddScoped<ILeaderboardService, LeaderboardService>();        services.AddScoped<IFriendsService, FriendsService>();    })    .Build();host.Run();
+var AllowSpecificOrigins = "_allowSpecificOrigins";
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.AddCors(options =>
+        {
+            options.AddPolicy(name: AllowSpecificOrigins,
+                              builder =>
+                              {
+                                  builder.WithOrigins(Environment.GetEnvironmentVariable("Host:CORS")).AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+                              });
+        });
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IActivityRepository, ActivityRepository>();
+        services.AddScoped<IBadgeRepository, BadgeRepository>();
+        services.AddScoped<ILeaderboardRepository, LeaderboardRepository>();
+        services.AddScoped<IFriendsRepository, FriendsRepository>();
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IActivityService, ActivityService>();
+        services.AddScoped<IBadgeService, BadgeService>();
+        services.AddScoped<ILeaderboardService, LeaderboardService>();
+        services.AddScoped<IFriendsService, FriendsService>();
+    })
+
+    .Build();
+
+host.Run();
